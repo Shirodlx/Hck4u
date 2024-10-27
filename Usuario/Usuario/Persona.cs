04cb78fab9223b965c70a460c4c427b39b5d05e2
Yashro <167231438+Shirodlx@users.noreply.github.com>
2024-10-26 22:34:45 -0300

    Add files via upload
    Estructura
    -List
    -While
    -Switch
    -If
    -Try/catch

---
+++ Usuario/Usuario/Persona.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuario
+{
+    public class Persona
+    {
+        private List<Persona> personas = new List<Persona>();
+        public string Nombre { get; set; }
+        public string Rut { get; set; }
+        public int Edad { get; set; }
+
+        public Persona() { }
+
+        public void CrearPersona()
+        {
+            try
+            {
+                Persona persona = new Persona();
+                Console.Write("Nombre: ");
+                persona.Nombre = Console.ReadLine();
+                Console.Write("Rut: ");
+                persona.Rut = Console.ReadLine();
+                Console.Write("Edad: ");
+                persona.Edad = int.Parse(Console.ReadLine());
+                personas.Add(persona);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato no correcto");
+            }
+        }
+
+        public void MostrarPersonas()
+        {
+            Console.WriteLine("\nLista de personas ingresadas:");
+            for (int i = 0; i < personas.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {personas[i].Nombre}, {personas[i].Rut}, {personas[i].Edad}");
+            }
+        }
+        public void ModificarNombre()
+        {
+            MostrarPersonas();
+            Console.Write("Selecciona el número de la persona a modificar su Nombre: ");
+            try
+            {
+                int index = int.Parse(Console.ReadLine()) - 1;
+                if (index < 0 || index >= personas.Count)
+                {
+                    Console.WriteLine("Índice fuera de rango.");
+                    return;
+                }
+
+                Console.Write("Nuevo nombre: ");
+                string nuevoNombre = Console.ReadLine();
+                personas[index].Nombre = nuevoNombre;
+                Console.WriteLine("Nombre modificado correctamente.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato no correcto.");
+            }
+        }
+        public void ModificarRut()
+        {
+            MostrarPersonas();
+            Console.Write("Selecciona el número de la persona a modificar su Rut: ");
+            try
+            {
+                int index = int.Parse(Console.ReadLine()) - 1;
+                if (index < 0 || index >= personas.Count)
+                {
+                    Console.WriteLine("Índice fuera de rango.");
+                    return;
+                }
+
+                Console.Write("Nuevo Rut: ");
+                string nuevoRut = Console.ReadLine();
+                personas[index].Rut = nuevoRut;
+                Console.WriteLine("Rut modificado correctamente.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato no correcto.");
+            }
+        }
+        public void ModificarEdad()
+        {
+            MostrarPersonas();
+            Console.Write("Selecciona el número de la persona a modificar su Edad: ");
+            try
+            {
+                int index = int.Parse(Console.ReadLine()) - 1;
+                if (index < 0 || index >= personas.Count)
+                {
+                    Console.WriteLine("Índice fuera de rango.");
+                    return;
+                }
+
+                Console.Write("Nueva Edad: ");
+                int nuevaEdad = int.Parse(Console.ReadLine());
+                personas[index].Edad = nuevaEdad;
+                Console.WriteLine("Rut modificado correctamente.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formato no correcto.");
+            }
+        }
+    }
+}
+
---
+++ Usuario/Usuario/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string continuar = "si";
+            Persona persona = new Persona();
+            while (continuar.ToLower() == "si")
+            {
+                
+                Console.WriteLine("Eligue una opción:");
+                Console.WriteLine("1. Crear persona.");
+                Console.WriteLine("2. Mostrar personas.");
+                Console.WriteLine("3. Modificar Atributo.");
+                Console.WriteLine("4. SALIR.");
+                string opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        persona.CrearPersona();
+                        break;
+                    case "2":
+                        persona.MostrarPersonas();
+                        break;
+                    case "3":
+                        Console.WriteLine("¿Qué deseas modificar?");
+                        Console.WriteLine("1. Nombre.");
+                        Console.WriteLine("2. Rut");
+                        Console.WriteLine("3. Edad)");
+                        int modificar = Convert.ToInt32(Console.ReadLine());
+                        if (modificar == 1)
+                        {
+                            persona.ModificarNombre();
+                        }
+                        else if (modificar == 2)
+                        {
+                            persona.ModificarRut();
+                        }
+                        else if (modificar == 3)
+                        {
+                            persona.ModificarEdad();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Opción no valida.");
+                        }
+                        break;
+
+                    case "4":
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción no válida. Por favor, elige una opción del 1 al 4.");
+                        break;
+                }
+                Console.WriteLine("¿Deseas realizar otra operación? (si/no):");
+                continuar = Console.ReadLine();
+            }
+        }
+    }
+}
